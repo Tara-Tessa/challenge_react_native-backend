d1882803ba1ef2964ba5838dd175c34d98cf1bc5
Tara-Tessa <tara-tessa.tesch@student.howest.be>
2021-10-18 15:34:22 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a61e831e549fed13cc8d0731a36b0400'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0597584-9c1b-4f07-8a4d-8d7b29599506'
+};
